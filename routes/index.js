f07189b8d5e3ad445921a4e43f3d59e8830acd61
Dagestan05 <steklofan@gmail.com>
2018-12-31 08:49:13 +0100

    added ejs viewEngine, created views

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-  res.send("Welcome to the brpwser")
+  res.render('welcome')
 })
 module.exports =router;
--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,12 @@ const router = express.Router();
 
 //Login page
 router.get("/login", (req, res)=>{
-  res.send("Welcome to login")
+  res.render("login")
 })
 
 //Register page
 router.get("/register", (req, res)=>{
-  res.send("Welcome to register")
+  res.render("register")
 })
 
 
