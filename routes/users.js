1e3af2de2d0be8f488b108c4223fc30015609a10
Dagestan05 <steklofan@gmail.com>
2019-01-01 09:08:31 +0100

    created logout route, ensureAuthentication MW

--- routes/users.js
+++ routes/users.js
@@ -88,4 +88,12 @@ router.post("/login", (req, res, next)=>{
   })(req, res, next)
 })
 
+//LogOut handle
+router.get("/logout", (req, res)=>{
+  //req.logout from passport lib
+  req.logOut();
+  req.flash("success_msg", "You are logged out")
+  res.redirect("/users/login")
+})
+
 module.exports =router;
