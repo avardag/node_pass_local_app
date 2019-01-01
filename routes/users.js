274060f1c8bc41d3530cdbf24382e33d2006fa5d
Dagestan05 <steklofan@gmail.com>
2019-01-01 08:55:55 +0100

    created login post route using passport localStartegy MW, added flash message

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const passport = require('passport');
 
 const User = require("../models/User");
 
@@ -75,6 +76,16 @@ router.post("/register", (req, res)=>{
         }
       })
   }
+})
+
+//Login handle
+router.post("/login", (req, res, next)=>{
+  //use passport local strategy
+  passport.authenticate('local', {
+    successRedirect: "/dashboard",
+    failureRedirect: "/users/login",
+    failureFlash: true
+  })(req, res, next)
 })
 
 module.exports =router;
